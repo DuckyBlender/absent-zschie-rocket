e05456248117acea71c39f5c816801d418b24b7a
DuckyBlender <alan@alanklas.com>
2022-11-16 20:27:53 +0100

    kinda works i guess

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,50 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use]
 extern crate rocket;
-use rocket_contrib::json::Json;
-use serde::{Serialize, Deserialize};
-use reqwest::blocking::Client;
 
-#[derive(Debug, Serialize)]
-struct StatusMessage {
-    status: u16
-}
+// import json from rocket
+use rocket::fs::NamedFile;
+use rocket::tokio::io::AsyncWriteExt;
+use tokio::stream;
 
 #[get("/get/<day>/<month>")]
-fn get(day: u8, month: u8) -> Json<StatusMessage> {
+async fn get(day: u8, month: u8) -> Result<NamedFile, String> {
     // Get current date
     let current_date = chrono::Local::now();
     // Format the current date to the PL format
     let current_date = current_date.format("%d.%m.%Y").to_string();
     // Send a get request to the server
 
-    let response = reqwest::blocking::get(format!("https://zastepstwa.zschie.pl/pliki/{}.pdf", current_date));
-    
+    let response = reqwest::get(format!(
+        "https://zastepstwa.zschie.pl/pliki/{}.pdf",
+        current_date
+    ))
+    .await
+    .unwrap();
+
     // If the server returns a 200 status code
     if response.status() == 200 {
-        return Json(StatusMessage { status: 200 });
+        // Create a new file
+        let mut file = rocket::tokio::fs::File::create("zastepstwa.pdf").await.unwrap();
+        // Download the PDF
+        let filebytes = response.bytes().await.unwrap();
+        // Write the PDF to the file
+        file.write_all(&filebytes).await.unwrap();
+        // Return the file
+        Ok(NamedFile::open("zastepstwa.pdf").await.unwrap())
+
     } else {
         // Return an error
         let response_status: u16 = response.status().as_u16();
-        return Json(StatusMessage { status: response_status });
+        Err(format!("Server returned a {} status code", response_status))
     }
 }
 
 #[get("/")]
-fn index() -> &'static str {
+async fn index() -> &'static str {
     "Hello, world!"
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![index, get]).launch();
+#[launch]
+fn launch() -> _ {
+    rocket::build().mount("/", routes![index, get])
 }

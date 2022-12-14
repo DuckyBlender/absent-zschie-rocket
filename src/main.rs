2fbd8b2c528ba00f74ce38a0667ece985cfdb7a1
DuckyBlender <alan@alanklas.com>
2022-12-14 01:18:54 +0100

    actually, scrap that, added a full fledged logger

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,14 @@ use chrono::Datelike;
 use rocket::fs::NamedFile;
 use rocket::tokio::io::AsyncWriteExt;
 
+use log::{error, info, warn};
+
+
 #[get("/?<day>&<month>")]
 async fn get_data(day: u8, month: u8) -> Result<NamedFile, String> {
+    info!("Incoming request for {}.{}", day, month);
     if day > 31 || month > 12 {
+        warn!("Invalid date: {}/{}", day, month);
         return Err("Invalid date".to_string());
     }
     // Make the day have 2 digits
@@ -18,46 +23,43 @@ async fn get_data(day: u8, month: u8) -> Result<NamedFile, String> {
     // Check if the file is in the cache
     let filename_pdf = format!("./cached/{}.pdf", date);
     if std::path::Path::new(&filename_pdf).exists() {
-        // Check if the file is younger then 15 minutes
-        let metadata = rocket::tokio::fs::metadata(&filename_pdf).await.expect("Error while getting metadata");
-        let file_age = chrono::Local::now() - chrono::DateTime::from(metadata.modified().expect("Error while getting file modified date"));
-        if file_age.num_minutes() < 15 {
+        // Check if the file is younger then 10 minutes
+        let metadata = rocket::tokio::fs::metadata(&filename_pdf)
+            .await
+            .expect("Error while getting metadata");
+        let file_age = chrono::Local::now()
+            - chrono::DateTime::from(
+                metadata
+                    .modified()
+                    .expect("Error while getting file modified date"),
+            );
+        if file_age.num_minutes() < 10 {
             // Return the file
-            return Ok(NamedFile::open(&filename_pdf).await.expect("Error while opening file"));
-        }
-        else {
+            info!("Returning cached data for {}", date);
+            return Ok(NamedFile::open(&filename_pdf)
+                .await
+                .expect("Error while opening file"));
+        } else {
             // Delete the file
-            rocket::tokio::fs::remove_file(&filename_pdf).await.expect("Error while deleting file");
+            info!("Deleting old cached data for {}", date);
+            rocket::tokio::fs::remove_file(&filename_pdf)
+                .await
+                .expect("Error while deleting file");
             // And continue as normal
         }
     }
 
-    let response = match reqwest::get(format!(
-        "https://zastepstwa.zschie.pl/pliki/{}.pdf",
-        date
-    ))
-    .await
-    {
-        Ok(response) => response,
-        Err(err) => return Err(format!("Error while fetching data: {}", err)),
-    };
+    info!("Getting data for {}", date);
+    let response =
+        match reqwest::get(format!("https://zastepstwa.zschie.pl/pliki/{}.pdf", date)).await {
+            Ok(response) => response,
+            Err(err) => return Err(format!("Error while fetching data: {}", err)),
+        };
 
     // If the server returns a 200 status code
     if response.status() == 200 {
-        
         // Create a new file
         let filename_pdf = format!("./cached/{}.pdf", date);
-
-        // Get the current time for the archive
-        let current_time = chrono::Local::now();
-        // Format the current time to the PL format
-        let exact_date = current_time.format("%d-%m-%Y---%H-%M-%S").to_string();
-        let archived_pdf = format!("./archive/{}.pdf", &exact_date);
-        // Save the file to the archive
-        let mut archive_file = match rocket::tokio::fs::File::create(&archived_pdf).await {
-            Ok(file) => file,
-            Err(err) => return Err(format!("Error while creating file: {}", err)),
-        };
 
         let mut file = match rocket::tokio::fs::File::create(&filename_pdf).await {
             Ok(file) => file,
@@ -74,15 +76,12 @@ async fn get_data(day: u8, month: u8) -> Result<NamedFile, String> {
             Err(err) => return Err(format!("Error while writing file: {}", err)),
         };
 
-        // Write the PDF to the archive
-        match archive_file.write_all(&filebytes).await {
-            Ok(file) => file,
-            Err(err) => return Err(format!("Error while writing file: {}", err)),
-        };
-
         // Return the PDF
-        Ok(NamedFile::open(&filename_pdf).await.expect("Error while opening file"))
+        Ok(NamedFile::open(&filename_pdf)
+            .await
+            .expect("Error while opening file"))
     } else if response.status() == 404 {
+        warn!("No data for {}", date);
         // If the server returns a 404 status code
         Err(format!(
             "Nie ma obecnie zastępstw na dzień {}! Spróbuj ponownie później!",
@@ -91,6 +90,7 @@ async fn get_data(day: u8, month: u8) -> Result<NamedFile, String> {
     } else {
         // Return an error
         let response_status = response.status().as_u16();
+        error!("Server returned a {} status code", response_status);
         Err(format!("Server returned a {} status code", response_status))
     }
 }
@@ -102,7 +102,9 @@ async fn auto_get_data(when: String) -> Result<NamedFile, String> {
         // If it's friday or saturday return message
         match chrono::Local::now().weekday() {
             chrono::Weekday::Fri => return Err("Jest jutro sobota, nie ma zastępstw!".to_string()),
-            chrono::Weekday::Sat => return Err("Jest jutro niedziela, nie ma zastępstw!".to_string()),
+            chrono::Weekday::Sat => {
+                return Err("Jest jutro niedziela, nie ma zastępstw!".to_string())
+            }
             _ => chrono::Local::now() + chrono::Duration::days(1),
         }
     } else if when == "today" {
@@ -118,6 +120,12 @@ async fn auto_get_data(when: String) -> Result<NamedFile, String> {
     } else {
         return Err("Invalid type in request".to_string());
     };
+    info!(
+        "Incoming request for {} ({}.{})",
+        when,
+        current_date.day(),
+        current_date.month()
+    );
 
     // Format the current date to the PL format
     let date = current_date.format("%d.%m.%Y").to_string();
@@ -126,45 +134,43 @@ async fn auto_get_data(when: String) -> Result<NamedFile, String> {
     // Check if the file is in the cache
     let filename_pdf = format!("./cached/{}.pdf", date);
     if std::path::Path::new(&filename_pdf).exists() {
-        // Check if the file is younger then 15 minutes
-        let metadata = rocket::tokio::fs::metadata(&filename_pdf).await.expect("Error while getting metadata");
-        let file_age = chrono::Local::now() - chrono::DateTime::from(metadata.modified().expect("Error while getting file modified date"));
-        if file_age.num_minutes() < 15 {
+        // Check if the file is younger then 10 minutes
+        let metadata = rocket::tokio::fs::metadata(&filename_pdf)
+            .await
+            .expect("Error while getting metadata");
+        let file_age = chrono::Local::now()
+            - chrono::DateTime::from(
+                metadata
+                    .modified()
+                    .expect("Error while getting file modified date"),
+            );
+        if file_age.num_minutes() < 10 {
             // Return the file
-            return Ok(NamedFile::open(&filename_pdf).await.expect("Error while opening file"));
-        }
-        else {
+            info!("Returning cached data for {}", date);
+            return Ok(NamedFile::open(&filename_pdf)
+                .await
+                .expect("Error while opening file"));
+        } else {
             // Delete the file
-            rocket::tokio::fs::remove_file(&filename_pdf).await.expect("Error while deleting file");
+            info!("Deleting old cached data for {}", date);
+            rocket::tokio::fs::remove_file(&filename_pdf)
+                .await
+                .expect("Error while deleting file");
             // And continue as normal
         }
     }
-    let response = match reqwest::get(format!(
-        "https://zastepstwa.zschie.pl/pliki/{}.pdf",
-        date
-    ))
-    .await
-    {
-        Ok(response) => response,
-        Err(err) => return Err(format!("Error while fetching data: {}", err)),
-    };
+
+    info!("Getting data for {}", date);
+    let response =
+        match reqwest::get(format!("https://zastepstwa.zschie.pl/pliki/{}.pdf", date)).await {
+            Ok(response) => response,
+            Err(err) => return Err(format!("Error while fetching data: {}", err)),
+        };
 
     // If the server returns a 200 status code
     if response.status() == 200 {
         // Create a new file
         let filename_pdf = format!("./cached/{}.pdf", date);
-
-        // Get the current time for the archive
-        let current_time = chrono::Local::now();
-        // Format the current time to the PL format
-        let exact_date = current_time.format("%d-%m-%Y---%H-%M-%S").to_string();
-        let archived_pdf = format!("./archive/{}.pdf", &exact_date);
-
-        // Save the file to the archive
-        let mut archive_file = match rocket::tokio::fs::File::create(&archived_pdf).await {
-            Ok(file) => file,
-            Err(err) => return Err(format!("Error while creating file: {}", err)),
-        };
 
         let mut file = match rocket::tokio::fs::File::create(&filename_pdf).await {
             Ok(file) => file,
@@ -181,12 +187,6 @@ async fn auto_get_data(when: String) -> Result<NamedFile, String> {
             Err(err) => return Err(format!("Error while writing file: {}", err)),
         };
 
-        // Write the PDF to the archive
-        match archive_file.write_all(&filebytes).await {
-            Ok(file) => file,
-            Err(err) => return Err(format!("Error while writing file: {}", err)),
-        };
-
         // Return the file
         match NamedFile::open(&filename_pdf).await {
             Ok(file) => Ok(file),
@@ -194,6 +194,7 @@ async fn auto_get_data(when: String) -> Result<NamedFile, String> {
         }
     } else if response.status() == 404 {
         // If the server returns a 404 status code
+        warn!("No data for {}", date);
         Err(format!(
             "Nie ma obecnie zastępstw na dzień {}! Spróbuj ponownie później!",
             date
@@ -201,6 +202,7 @@ async fn auto_get_data(when: String) -> Result<NamedFile, String> {
     } else {
         // Return an error
         let response_status = response.status().as_u16();
+        error!("Server returned a {} status code", response_status);
         Err(format!("Server returned a {} status code", response_status))
     }
 }
@@ -216,13 +218,49 @@ async fn launch() -> _ {
     // Check if the cached folder exists
     if !std::path::Path::new("./cached").exists() {
         // If it doesn't, create it
-        rocket::tokio::fs::create_dir("./cached").await.expect("Error while creating cached folder");
+        rocket::tokio::fs::create_dir("./cached")
+            .await
+            .expect("Error while creating cached folder");
     }
-    // Check if archive folder exists
-    if !std::path::Path::new("./archive").exists() {
+    // Check if the log file exists
+    if !std::path::Path::new("./log.yml").exists() {
         // If it doesn't, create it
-        rocket::tokio::fs::create_dir("./archive").await.expect("Error while creating archive folder");
+        rocket::tokio::fs::File::create("./log.yml")
+            .await
+            .expect("Error while creating log file");
+        // And write the default config to it
+        // https://tms-dev-blog.com/log-to-a-file-in-rust-with-log4rs/
+        let config = r#"
+        appenders:  
+            my_stdout:
+                kind: console
+                encoder:
+                    pattern: "{h({d(%Y-%m-%d %H:%M:%S)(utc)} - {l}: {m}{n})}"
+            my_file_logger:
+                kind: rolling_file
+                path: "log/log.log"
+                encoder:
+                    pattern: "{d(%Y-%m-%d %H:%M:%S)(utc)} - {h({l})}: {m}{n}"
+                policy:
+                    trigger:
+                        kind: size
+                        limit: 50kb
+                    roller:
+                        kind: fixed_window
+                        base: 1
+                        count: 10
+                        pattern: "log/log{}.log"
+        root:
+            level: info
+            appenders:
+                - my_stdout
+                - my_file_logger"#;
+        rocket::tokio::fs::write("./log.yml", config)
+            .await
+            .expect("Error while writing to log file");
     }
+    // Set the logger
+    log4rs::init_file("./log.yml", Default::default()).expect("Error while setting logger");
 
     // Start the server
     rocket::build()
